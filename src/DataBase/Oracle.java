1f25a964fd0fbb1ec2da8856ad4d3462a7f23cb7
w <dania.tsr@mail.ru>
2024-02-09 19:13:22 +0300

    ошибка в докерфайле

--- src/DataBase/Oracle.java
+++ src/DataBase/Oracle.java
@@ -1,6 +1,5 @@
 package DataBase;
 import MainActivity.Main;
-
 public class Oracle {
     public static final String url = "jdbc:oracle:thin:@localhost:1521/"+ Main.nameDataBase;
     public static final String user = "Danila";
--- src/DataBase/Postgresql.java
+++ src/DataBase/Postgresql.java
@@ -1,7 +1,7 @@
 package DataBase;
 import MainActivity.Main;
 public class Postgresql {
-    public static final String url = "jdbc:postgresql://localhost:5432/"+ Main.nameDataBase;
+    public static final String url = "jdbc:postgresql://192.168.100.184:5432/"+ Main.nameDataBase;
     public static final String user = "postgres";
     public static final String password = "2691";//
 
